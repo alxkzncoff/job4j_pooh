9efe876301bbef3c878b5dc3e7ed6d14058a5830
Aleksandr <alx.kzncoff@gmail.com>
2022-04-11 14:44:15 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841 #283167]

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -3,27 +3,59 @@ package ru.job4j.pooh;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * Отправитель посылает запрос на добавление данных с указанием очереди (weather)
+ * и значением параметра (temperature=18). Сообщение помещается в конец очереди.
+ * Если очереди нет в сервисе, то нужно создать новую и поместить в нее сообщение.
+ *
+ * Получатель посылает запрос на получение данных с указанием очереди.
+ * Сообщение забирается из начала очереди и удаляется.
+ *
+ * Если в очередь приходят несколько получателей, то они поочередно получают сообщения из очереди.
+ *
+ * Каждое сообщение в очереди может быть получено только одним получателем.
+ * @author Aleksandr Kuznetsov.
+ * @version 1.0
+ */
+
 public class QueueService implements Service {
 
     private final ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
-        return switch (req.httpRequestType()) {
-            case "POST" -> {
-                queue.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
-                queue.get(req.getSourceName()).add(req.getParam());
-                yield new Resp("", "204");
-            }
-            case "GET" -> {
-                if (queue.isEmpty()) {
-                    yield new Resp("", "204");
-                } else {
-                    ConcurrentLinkedQueue<String> localQueue = queue.getOrDefault(req.getSourceName(), new ConcurrentLinkedQueue<>());
-                    yield localQueue.isEmpty() ? new Resp("", "201") : new Resp(localQueue.poll(), "200");
-                }
-            }
-            default -> new Resp("", "501");
-        };
+        Resp result = new Resp("", "501");
+        if ("POST".equals(req.httpRequestType())) {
+            result = post(req.getSourceName(), req.getParam());
+        } else if ("GET".equals(req.httpRequestType())) {
+            result = get(req.getSourceName());
+        }
+        return result;
+    }
+
+    /**
+     * Метод обрабаьтывает POST запросы.
+     * @param sourceName поле sourceName класса Req.
+     * @param param поле param класса Req.
+     * @return Resp результат обработки запроса.
+     */
+    private Resp post(String sourceName, String param) {
+        queue.putIfAbsent(sourceName, new ConcurrentLinkedQueue<>());
+        queue.get(sourceName).add(param);
+        return new Resp("", "200");
+    }
+
+    /**
+     * Метод обрабаотывает GET запросы.
+     * @param sourceName поле sourceName класса Req.
+     * @return Resp результат обработки запроса.
+     */
+    private Resp get(String sourceName) {
+        Resp result = new Resp("", "204");
+        String text = queue.getOrDefault(sourceName, new ConcurrentLinkedQueue<>()).poll();
+        if (text != null) {
+            result = new Resp(text, "200");
+        }
+        return result;
     }
 }
--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -3,27 +3,62 @@ package ru.job4j.pooh;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * Отправитель посылает запрос на добавление данных с указанием топика (weather)
+ * и значением параметра (temperature=18).
+ * Сообщение помещается в конец каждой индивидуальной очереди получателей.
+ * Если топика нет в сервисе, то данные игнорируются.
+ *
+ * Получатель посылает запрос на получение данных с указанием топика.
+ * Если топик отсутствует, то создается новый. А если топик присутствует,
+ * то сообщение забирается из начала индивидуальной очереди получателя и удаляется.
+ *
+ * Когда получатель впервые получает данные из топика – для него создается индивидуальная пустая очередь.
+ * Все последующие сообщения от отправителей с данными для этого топика помещаются в эту очередь тоже.
+ * @author Aleksandr Kuznetsov.
+ * @version 1.0
+ */
+
 public class TopicService implements Service {
     private final ConcurrentHashMap<String,
             ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> topics = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
-        return switch (req.httpRequestType()) {
-            case "POST" -> {
-                ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> topic = topics.getOrDefault(req.getSourceName(), new ConcurrentHashMap<>());
-                for (ConcurrentLinkedQueue<String> queue: topic.values()) {
-                    queue.add(req.getParam());
-                }
-                yield new Resp("", "204");
-            }
-            case "GET" -> {
-                topics.putIfAbsent(req.getSourceName(), new ConcurrentHashMap<>());
-                topics.get(req.getSourceName()).putIfAbsent(req.getParam(), new ConcurrentLinkedQueue<>());
-                String text = topics.get(req.getSourceName()).get(req.getParam()).poll();
-                yield text == null ? new Resp("", "204") : new Resp(text, "200");
-            }
-            default -> new Resp("", "501");
-        };
+        Resp result = new Resp("", "501");
+        if ("POST".equals(req.httpRequestType())) {
+            result = post(req.getSourceName(), req.getParam());
+        } else if ("GET".equals(req.httpRequestType())) {
+            result = get(req.getSourceName(), req.getParam());
+        }
+        return result;
+    }
+
+    /**
+     * Метод обрабатывает POST запросы.
+     * @param sourceName поле sourceName класса Req.
+     * @param param поле param класса Req.
+     * @return Resp результат обработки запроса.
+     */
+    private Resp post(String sourceName, String param) {
+        ConcurrentHashMap<String,
+                ConcurrentLinkedQueue<String>> topic = topics.getOrDefault(sourceName, new ConcurrentHashMap<>());
+        for (ConcurrentLinkedQueue<String> queue : topic.values()) {
+            queue.add(param);
+        }
+        return new Resp("", "204");
+    }
+
+    /**
+     * Метод обрабаотывает GET запросы.
+     * @param sourceName поле sourceName класса Req.
+     * @param param поле param класса Req.
+     * @return Resp результат обработки запроса.
+     */
+    private Resp get(String sourceName, String param) {
+        topics.putIfAbsent(sourceName, new ConcurrentHashMap<>());
+        topics.get(sourceName).putIfAbsent(param, new ConcurrentLinkedQueue<>());
+        String text = topics.get(sourceName).get(param).poll();
+        return text != null ? new Resp(text, "200") : new Resp("", "204");
     }
 }
